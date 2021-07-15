6d88106fa57798da466268c49009244199cf900a
davidchdebray <58414118+davidchdebray@users.noreply.github.com>
2021-07-15 09:33:41 +0200

    Update POEI AWS pdf doc

--- DocsBuild.sh
+++ DocsBuild.sh
@@ -6,7 +6,7 @@ echo " " > nav.txt
 ls -R | grep "md" > nav.txt
 sed -i -e 's/.md//g' nav.txt
 
-for n in $(cat nav.txt); do pandoc $n.md -s -c styleLight.css -A footer.html --toc -o KnowledgeBase/$n.html; done
+for n in $(cat nav.txt); do pandoc $n.md -s -c styleDark.css -A footer.html --toc -o KnowledgeBase/$n.html; done
 
 echo ""
 echo "" 
